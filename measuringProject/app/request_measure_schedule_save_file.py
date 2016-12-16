6566f8ecf9fcd5ab2031d73a4b2581f7b1afe443
Isaque Fontinele <isaquefontinele@gmail.com>
2016-12-16 01:50:00 -0300

    Aplicativo cliente atualizado

--- measuringProject/app/request_measure_schedule_save_file.py
+++ measuringProject/app/request_measure_schedule_save_file.py
@@ -2,6 +2,7 @@ import requests
 import time
 import schedule
 import csv
+import psutil
 
 num_ensaios = 3
 contador_execucoes = 0
@@ -9,28 +10,20 @@ registro_tempos = []
 
 def job():
     global contador_execucoes
-    start_time = time.time()
-
-    r = requests.get('https://api.github.com/events')
-    # exemplos de outros requests
-    # r = requests.post('http://httpbin.org/post', data = {'key':'value'})
-    # r = requests.put('http://httpbin.org/put', data = {'key':'value'})
-    # r = requests.delete('http://httpbin.org/delete')
-    # r = requests.head('http://httpbin.org/get')
-    # r = requests.options('http://httpbin.org/get')
-    
-    print r.status_code
-    print r.headers['content-type']
+    pids = psutil.pids()
+    p = psutil.Process(pids[-1])
     contador_execucoes += 1;
-    registro_tempos.append([time.time() - start_time])
-   
+    
+    start_time = time.time()
+    r = requests.get('https://api.github.com/events')
+    registro_tempos.append(('', contador_execucoes, p.name(), time.time() - start_time, p.memory_percent(), p.cpu_percent(interval=1.0), ''))
+    
+##    print r.status_code
+##    print r.headers['content-type']    
+##    print("\nNome do processo: %s\n\nUso de memoria: \n%s\n\nUso de CPU: \n%s" % (p.name(), p.memory_percent(), p.cpu_percent(interval=1.0)))    
+    
 
 schedule.every(2).seconds.do(job)
-# exemplos de outros schedules
-# schedule.every().hour.do(job)
-# schedule.every().day.at("10:30").do(job)
-# schedule.every().monday.do(job)
-# schedule.every().wednesday.at("13:15").do(job)
 
 #main loop
 while True:
@@ -45,5 +38,20 @@ while True:
 with open('the_file.csv', 'wb') as csvfile:
     writer = csv.writer(csvfile, delimiter=' ',
                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    for row in registro_tempos:
+    for row in zip(registro_tempos):
         writer.writerow(row)
+
+
+
+# exemplos de outros requests
+# r = requests.post('http://httpbin.org/post', data = {'key':'value'})
+# r = requests.put('http://httpbin.org/put', data = {'key':'value'})
+# r = requests.delete('http://httpbin.org/delete')
+# r = requests.head('http://httpbin.org/get')
+# r = requests.options('http://httpbin.org/get')   
+
+# exemplos de outros schedules
+# schedule.every().hour.do(job)
+# schedule.every().day.at("10:30").do(job)
+# schedule.every().monday.do(job)
+# schedule.every().wednesday.at("13:15").do(job)
