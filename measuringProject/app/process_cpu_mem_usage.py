e2fddb0f3d3df5c6f81913b79f5b718de68d6e4a
victorbc <vbatistacamilo@gmail.com>
2017-03-01 12:41:13 -0300

    Now the script looks for all process with name equals <arg> and sum their cpu and memory

--- measuringProject/app/process_cpu_mem_usage.py
+++ measuringProject/app/process_cpu_mem_usage.py
@@ -1,5 +1,23 @@
 import psutil
-pids = psutil.pids()
-p = psutil.Process(pids[-1])
-print("\nNome do processo: %s\n\nUso de memoria: \n%s\n\nUso de CPU: \n%s" % (p.name(), p.memory_percent(), p.cpu_percent(interval=1.0)))
+import sys
+
+if(len(sys.argv) == 1):
+	print "Passe o nome do processo como argumento"
+else:
+	pids = psutil.pids()
+
+	PROCNAME = str(sys.argv[1])
+	procdata = [0,0]
+
+	for proc in psutil.process_iter():
+	    if proc.name() == PROCNAME:
+	    	procdata[0] += proc.memory_percent();
+	    	procdata[1] += proc.cpu_percent(interval=1.0);
+
+
+
+	print("\nNome do processo: %s\n\nUso de memoria: \n%s\n\nUso de CPU: \n%s" % 
+	        	(PROCNAME, procdata[0], procdata[1]))
+
+
 
